89aae1b956e2661c07344298478ee6485735e23c
Daniel Milroy <daniel.milroy@colorado.edu>
2014-09-17 13:48:51 -0600

    Creating new branch, modified to submit jobs in Slurm.

--- remotenb/remote_notebook.py
+++ remotenb/remote_notebook.py
@@ -9,20 +9,21 @@ import jinja2 as jin
 import connection as co
 import tunnel as tn
 
-template = jin.Template('''
-#!/bin/bash
-#PBS -N ipython_notebook
-#PBS -q {{queue}}
-#PBS -l walltime={{walltime}}
-#PBS -l nodes={{nodes}}:ppn={{cores}}
-#PBS -j oe
-#PBS -o output-$PBS_JOBNAME-$PBS_JOBID
+template = jin.Template('''#!/bin/bash
+#SBATCH -J ipython_notebook
+#SBATCH --qos {{queue}}
+#SBATCH --time={{walltime}}
+#SBATCH -N {{nodes}}
+#SBATCH --ntasks-per-node {{cores}}
+#SBATCH --output output-ipython_nodebook-%j
 
 mkdir -p $HOME/.notebooks
-echo $HOSTNAME > $HOME/.notebooks/node.$PBS_JOBNAME.$PBS_JOBID
+echo $HOSTNAME > $HOME/.notebooks/node.ipython_notebook.$SLURM_JOB_ID
 
 mkdir -p {{directory}}
 cd {{directory}}
+
+. /etc/profile.d/modules.sh
 
 module load python/anaconda-1.9.1
 
@@ -40,12 +41,11 @@ class RemoteNotebook:
 
 		self.args['local_port'] = kwargs.get('local_port', 9999)
 		self.args['walltime'] = kwargs.get('walltime', '01:00:00')
-		self.args['queue'] = kwargs.get('queue', 'janus-debug')
+		self.args['queue'] = kwargs.get('queue', 'normal')
 		self.args['directory'] = kwargs.get('directory', '$HOME')
 		self.args['nodes'] = kwargs.get('nodes', 1)
 		self.args['pwd'] = kwargs.get('pwd', True)
 		self.args['remote_port'] = random.randint(9000,60000)
-		#self.args['remote_port'] = int(np.random.uniform(9000,60000,size=1)[0])
 		self.args['cores'] = 12
 		
 
@@ -64,28 +64,22 @@ class RemoteNotebook:
 		except IOError:
 			pass
 
-	def _write_pbs_script(self, rc):
+	def _write_slurm_script(self, rc):
 
-		pbs_script = template.render(self.args)
-		rc.sftp.open('.notebooks/ipython_notebook.pbs', 'w').write(pbs_script)
+		slurm_script = template.render(self.args)
+		rc.sftp.open('.notebooks/ipython_notebook.slurm', 'w').write(slurm_script)
 
-	def _start_pbs_job(self, rc):
+	def _start_slurm_job(self, rc):
 		print '\tsubmitting job'
-		_, output, _ = rc.ssh.exec_command('cd .notebooks; /curc/tools/free/redhat_6_x86_64/torque-4.2.3/bin/qsub ipython_notebook.pbs')
-		
-		jobid = None
-		for lines in output:
-			print '\t' + lines
-			m = re.match(r'([0-9]+).moab.rc.colorado.edu', lines)
-			if m:
-				jobid = m.group(1)
-				return jobid
+		_, output, _ = rc.ssh.exec_command('cd .notebooks; /curc/slurm/slurm/current/bin/sbatch ipython_notebook.slurm')
+		jobid = int(output.read().strip().split()[-1])
+		return jobid
 
 	def _get_nodename(self, rc):
 		print '\twaiting for job', self.jobid, 'to start'
 		while True:
 			try:
-				filename = '.notebooks/node.ipython_notebook.{0}.moab.rc.colorado.edu'.format(self.jobid)
+				filename = '.notebooks/node.ipython_notebook.{0}'.format(self.jobid)
 				nodename = rc.sftp.open(filename, 'r').read().strip()
 				return nodename
 			except IOError:
@@ -102,13 +96,12 @@ class RemoteNotebook:
 			print '\tnotebook closed'
 
 	def close(self):
-	
 		rc = co.Connection(self.args['hostname'], 
-						   username = self.args['hostname'],
-						   port = self.args['hostname'],
+						   username = self.args['username'],
+						   port = self.args['port'],
 						   pwd = self.args['pwd'])
 
-		cmd = '/curc/tools/free/redhat_6_x86_64/torque-4.2.3/bin/qdel {0}'.format(self.jobid)
+		cmd = '/curc/slurm/slurm/current/bin/scancel {0}'.format(int(self.jobid))
 		rc.ssh.exec_command(cmd)
 
 		cmd = 'rm .notebooks/*'
@@ -134,8 +127,8 @@ class RemoteNotebook:
 							self.args['port'])
 		print cstr
 		self._create_notebooks_dir(rc)
-		self._write_pbs_script(rc)
-		self.jobid = self._start_pbs_job(rc)
+		self._write_slurm_script(rc)
+		self.jobid = self._start_slurm_job(rc)
 		self.nodename = self._get_nodename(rc)
 		rc.close()
 	    
@@ -157,15 +150,8 @@ if __name__ == '__main__':
 
 	rn = RemoteNotebook(hostname='login', 
 						directory='/projects/molu8455/notebooks', 
-						queue='janus-admin', 
+						queue='normal', 
 						pwd=False)
 
 	rn.connect() # Waits
-
-
-
-
-
-
-
 
